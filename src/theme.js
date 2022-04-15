5cb60068176ac72a3c810e107d4ab31365b1c5f2
unknown <haseebnk37@gmail.com>
2022-04-15 10:45:54 +0500

    first commit

---
+++ src/theme.js
@@ -0,0 +1,37 @@
+// Theme Presets
+
+const lightTheme = {
+  name: "light",
+  body: "#FFEEEE",
+  text: "#343434",
+  dark: "#000000",
+  secondaryText: "#7F8DAA",
+  accentColor: "#E3405F",
+  accentBright: "#E3405F",
+  projectCard: "#DCE4F2",
+  skinColor: "#F7B799",
+  skinColor2: "#FCB696",
+  imageDark: "#dce4f2",
+  imageClothes: "#dce4f2",
+  avatarMisc: "#e9ecf2",
+  avatarShoes: "#ccd2e3",
+};
+
+const darkTheme = {
+  name: "dark",
+  body: "#0F0E0E",
+  text: "#FFFFFF",
+  dark: "#000000",
+  secondaryText: "#8D8D8D",
+  accentColor: "#E3405F",
+  accentBright: "#E3405F",
+  projectCard: "#292A2D",
+  skinColor: "#F7B799",
+  skinColor2: "#FCB696",
+  imageDark: "#292A2D",
+  imageClothes: "#000000",
+  avatarMisc: "#212121",
+  avatarShoes: "red",
+};
+
+export const themes = { light: lightTheme, dark: darkTheme };
